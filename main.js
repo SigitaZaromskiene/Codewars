feaf5f23a9cb34f79499f0b316a7522b52c42e08
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-02-13 11:50:24 +0200

    Codewars

--- main.js
+++ main.js
@@ -1,1 +1,72 @@
 console.log("Veikia");
+
+// let newArr = [];
+// function evenNumbers(array, number) {
+//   const filtered = array.filter((num) => num % 2 === 0);
+//   newArr.push(filtered);
+//   return filtered.slice(-number);
+// }
+
+// console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+
+// function usdcny(usd) {
+//   const money = usd * (6.75).toFixed(2);
+//   console.log(money);
+//   return `${money.toFixed(2)} Chinese Yuan`;
+// }
+
+// console.log(usdcny(15));
+// console.log(usdcny(6356));
+
+// const newArr = [];
+// const even = [];
+// const odd = [];
+// function rowWeights(array) {
+//   array.forEach((element, i) => {
+//     if (i % 2 === 0) {
+//       even.push(element);
+//     } else {
+//       odd.push(element);
+//     }
+//   });
+
+//   const oneEven = even.reduce((acc, num) => acc + num);
+//   const oneOdd = odd.reduce((acc, num) => acc + num);
+
+//   newArr.push(oneEven, oneOdd);
+//   return newArr;
+// }
+
+// console.log(rowWeights([50, 60, 70, 80]));
+
+// function reverseWords(str) {
+//   const newArr = [];
+//   const splited = str.split(" ");
+//   for (let i = 0; i < splited.length; i++) {
+//     const reversed = splited[i].split("").reverse().join("");
+//     newArr.push(reversed);
+//   }
+//   const joined = newArr.join(" ");
+//   return joined;
+// }
+
+// console.log(reverseWords("The quick brown fox jumps over the lazy dog."));
+
+// function noOdds(values) {
+//   const even = [];
+//   values.forEach((element, i) => {
+//     if (i % 2 !== 0) {
+//       even.push(element);
+//     }
+//   });
+//   return even;
+// }
+
+// console.log(noOdds([0, 1, 2, 3]));
+function basicOp(operation, value1, value2) {
+  const sign = `${opeation}`;
+  const ats = value1;
+  return sen;
+}
+
+console.log(basicOp("+", 4, 7));
